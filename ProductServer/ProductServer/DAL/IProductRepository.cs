20ac43a9d3c834890a7f726581e41c11a77addcd
Eimutisg <eimutis.genys@gmail.com>
2018-03-19 12:27:35 +0000

    DAL

---
+++ ProductServer/ProductServer/DAL/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using ProductServer.Models.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductServer.DAL
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        Task<IList<Product>> GetReorderList();
+        float GetStockCost(int ProductID);
+        bool CheckStock(int productID, int quantityPurchased);
+        Task<Product> OrderItem(Product p, int Quantity);
+    }
+}
---
+++ ProductServer/ProductServer/DAL/ISupplierRepository.cs
@@ -0,0 +1,14 @@
+﻿using ProductServer.Models.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductServer.DAL
+{
+    public interface ISupplierRepository : IRepository<Supplier>
+    {
+        Task<IList<Product>> SupplierProducts();
+    }
+}
