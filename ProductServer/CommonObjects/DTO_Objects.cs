c406e9bfc7ad81f0f18422068434f55d2a2d040d
Eimutisg <eimutis.genys@gmail.com>
2018-03-19 14:10:45 +0000

    DTO - Appropriate classes to support info being sent up to and returned from controller actions

---
+++ ProductServer/CommonObjects/DTO_Objects.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonObjects
+{
+    public class ProductDTO
+    {
+        public int ProductId { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public int ReorderLevel { get; set; }
+        public float Price { get; set; }
+        public int SupplierID { get; set; }
+        public SupplierDTO Supplier { get; set; }
+
+        public override string ToString()
+        {
+            return string.Concat(ProductId.ToString(), " ", Description, " ", Quantity.ToString(), SupplierID.ToString());
+        }
+    }
+
+    public class SupplierDTO
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public List<ProductDTO> Products { get; set; }
+        public override string ToString()
+        {
+            return string.Concat(ID.ToString(), " ", Name, " ", Address);
+        }
+    }
+}
