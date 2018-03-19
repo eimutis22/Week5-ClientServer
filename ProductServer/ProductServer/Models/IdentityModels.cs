f032994f35cbf2472b5db0c751ef7d01768a532d
Eimutisg <eimutis.genys@gmail.com>
2018-03-19 12:11:31 +0000

    Changed Connection in ApplicationDbContext

--- ProductServer/ProductServer/Models/IdentityModels.cs
+++ ProductServer/ProductServer/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace ProductServer.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("ProductDbContext", throwIfV1Schema: false)
         {
         }
         
