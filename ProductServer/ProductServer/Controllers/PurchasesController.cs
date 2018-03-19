772afc1ce1b89226acc0db2e27d6023f5b1475fd
Eimutisg <eimutis.genys@gmail.com>
2018-03-19 14:00:15 +0000

    Decorated with Routing Atribute

--- ProductServer/ProductServer/Controllers/PurchasesController.cs
+++ ProductServer/ProductServer/Controllers/PurchasesController.cs
@@ -8,6 +8,7 @@ using System.Web.Http;
 namespace ProductServer.Controllers
 {
     [Authorize(Roles = "PurchasesManager")]
+    [RoutePrefix("api/Purchases")]
     public class PurchasesController : ApiController
     {
     }
