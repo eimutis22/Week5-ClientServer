a37bc2803cc343480817fce56a9079e1a5ac60d4
Eimutisg <eimutis.genys@gmail.com>
2018-03-19 14:32:48 +0000

    ClientConsole
    1. Login and retrieve a token for the Product Manager.
    2. Add a new Supplier and associated Product for that supplier.
    3. Delete a supplier but not their listed products.

--- ProductServer/ClientConsole/Program.cs
+++ ProductServer/ClientConsole/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CommonObjects;
+using ProductClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,46 @@ namespace ClientConsole
     {
         static void Main(string[] args)
         {
+
+            // Login and set Token for a valid user account
+            ClientApiLib.baseWebAddress = "http://localhost:49614/";
+
+            if (ClientApiLib.login("fflynstone", "Flint$12345"))
+            {
+                List<ProductDTO> reorderList = ClientApiLib.getOrorderList();
+                foreach (var item in reorderList)
+                    Console.WriteLine("{0} {1} {2}", item.Description, item.ReorderLevel, item.Quantity);
+
+
+                // Create a new Product with associated supplier
+                ProductDTO newprod = new ProductDTO
+                {
+                    Description = "glass hammers",
+                    Price = 200f,
+                    Quantity = 2,
+                    ReorderLevel = 5,
+                    Supplier = new SupplierDTO { Name = "Supplier 1", Address = "1 Sup Road" }
+                };
+
+
+                // add the supplier and product. Console -> Product Client: Post ProductDTO -> Product Server: Purchases add/SupplierProduct Action
+                // return Product from Product Server. NOTE: json serialiser will map approriate fields to Product DTO on deserialsation based
+                // on naming convention of matching fields          
+                ProductDTO ProductInserted = ClientApiLib.addSupplierProduct(newprod);
+
+                // if it was inserted by the server 
+                if (ProductInserted != null)
+                    Console.WriteLine(ProductInserted.ToString());
+
+
+                // Delete the newly inserted supplier. Note if you want to stop 
+                SupplierDTO deleted = ClientApiLib.delete(ProductInserted.SupplierID);
+                if (deleted != null)
+                    Console.WriteLine(deleted.ToString() + " Was deleted ");
+
+                Console.ReadKey();
+            }
+
 
         }
     }
